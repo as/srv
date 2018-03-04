68b913728393844874716142ee559687d40751b8
as <as.utf8@gmail.com>
2018-03-03 17:11:13 -0800

    srv: local transactional file system

---
+++ fs/local.go
@@ -0,0 +1,20 @@
+package srv
+
+import (
+	"context"
+	"io/ioutil"
+	"os/exec"
+)
+
+type Local struct {
+}
+
+func (f *Local) Get(name string) (data []byte, err error) {
+	return ioutil.ReadFile(name)
+}
+func (f *Local) Put(name string, data []byte) (err error) {
+	return ioutil.WriteFile(name, data, 0666)
+}
+func (f *Local) Cmd(ctx context.Context, name string, arg ...string) (*exec.Cmd, error) {
+	return exec.CommandContext(ctx, name, arg...), nil
+}
