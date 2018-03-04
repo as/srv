0a00d9fcee3c096bfb9a2a8f6b614e9c1a72c6f3
as <as.utf8@gmail.com>
2018-03-03 17:11:44 -0800

    fs: write a simple interface for it

---
+++ fs/fs.go
@@ -0,0 +1,24 @@
+package srv
+
+import (
+	"context"
+	"io"
+	"os/exec"
+)
+
+type Command interface {
+	CombinedOutput() ([]byte, error)
+	Output() ([]byte, error)
+	Run() error
+	Start() error
+	StderrPipe() (io.ReadCloser, error)
+	StdinPipe() (io.WriteCloser, error)
+	StdoutPipe() (io.ReadCloser, error)
+	Wait() error
+}
+
+type Fs interface {
+	Get(name string) (data []byte, err error)
+	Put(name string, data []byte) (err error)
+	Cmd(ctx context.Context, name string, arg ...string) (*exec.Cmd, error)
+}
